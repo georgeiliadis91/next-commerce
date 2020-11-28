64b29f9ffd357596b6a9bf2754d97560f0763030
georgeiliadis91 <george.iliadis.91@gmail.com>
2020-11-28 21:34:25 +0200

    removed defaults

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Next Commerce</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <p> E Commerce </p>
     </div>
   );
 }
